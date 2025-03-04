cc91bcf8ae658f0870890d85be00e0d49afba2fc
danielsaiger1 <daniel.saiger@yahoo.de>
2025-03-04 18:19:01 +0100

    implemented token API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,6 +12,7 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 
+
 def create_user(**params):
     """Create and return a new user"""
     return get_user_model().objects.create_user(**params)
@@ -63,7 +64,7 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
-    
+
     def test_create_token_for_user(self):
         """Test generates token for valid credentials"""
         user_details = {
