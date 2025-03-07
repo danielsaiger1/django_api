69168887e35b82aeffe58915a1e558ff12d252c3
danielsaiger1 <daniel.saiger@yahoo.de>
2025-03-07 10:01:44 +0100

    added tests for recipe details endpoint

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -12,10 +12,19 @@ from rest_framework.test import APIClient
 
 from core.models import Recipe
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+    )
 
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+
+def detail_url(recipe_id):
+    """Create and return a recipe detail url"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
 
 def test_create_recipe(user, **params):
     """Create and return a sample recipe"""
@@ -78,4 +87,14 @@ class PrivateRecipeAPITests(TestCase):
         recipes = Recipe.objects.filter(user=self.user)
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """Test getting a recipe detail"""
+        recipe = test_create_recipe(user=self.user)
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer
         self.assertEqual(res.data, serializer.data)
