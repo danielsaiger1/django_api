8d3ac0b98115b90f0ff995c5fefe04141b0e3a41
danielsaiger1 <daniel.saiger@yahoo.de>
2025-03-17 19:12:44 +0100

    implementation of image upload functionality

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -414,11 +414,11 @@ class ImageUploadTests(TestCase):
     def test_upload_image(self):
         """Test uploading an image to an recipe"""
         url = image_upload_url(self.recipe.id)
-        with tempfile.NamedTemporaryFile(suffix='jpg') as image_file:
+        with tempfile.NamedTemporaryFile(suffix='.jpg') as image_file:
             img = Image.new('RGB', (10, 10))
             img.save(image_file, format='JPEG')
             image_file.seek(0)
-            payload = {'image', image_file}
+            payload = {'image': image_file}
             res = self.client.post(url, payload, format='multipart')
 
         self.recipe.refresh_from_db()
