73219b999300bb10b67b6597bb594a66a4fd2bca
danielsaiger1 <daniel.saiger@yahoo.de>
2025-03-17 18:40:15 +0100

    tests for uploading an image

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -407,7 +407,7 @@ class ImageUploadTests(TestCase):
 
         self.client.force_authenticate(self.user)
         self.recipe = test_create_recipe(user=self.user)
-    
+
     def tearDown(self):
         self.recipe.image.delete()
 
@@ -420,7 +420,7 @@ class ImageUploadTests(TestCase):
             image_file.seek(0)
             payload = {'image', image_file}
             res = self.client.post(url, payload, format='multipart')
-        
+
         self.recipe.refresh_from_db()
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn('image', res.data)
